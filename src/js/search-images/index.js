d00cf587c4d914b77cdfdb3977e425bbf420adaa
Lulu0502 <suliya85@gmail.com>
2022-10-23 19:40:56 +0300

    .

--- src/js/search-images/index.js
+++ src/js/search-images/index.js
@@ -84,6 +84,7 @@ async function onClickLoadMoreBtn() {
   lightbox.refresh();
   currentHits += response.hits.length;
 
+  
   if (currentHits === response.totalHits) {
     loadMoreBtn.classList.add('is-hidden');
     endCollectionText.classList.remove('is-hidden');
